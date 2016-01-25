43a8356f8c291d30fec507bb7dfcbdfc329fb7d7
ARIHARAN S <ariharan_s@mm-ariharan-s.local>
2016-01-25 18:16:55 +0530

    Alert Message Added

--- Pod/Classes/ClassUtilities.h
+++ Pod/Classes/ClassUtilities.h
@@ -10,4 +10,6 @@
 
 @interface ClassUtilities : NSObject
 
++(void)ShowAlert:(NSString*)message;
+
 @end
--- Pod/Classes/ClassUtilities.m
+++ Pod/Classes/ClassUtilities.m
@@ -7,7 +7,14 @@
 //
 
 #import "ClassUtilities.h"
+#import <UIKit/UIKit.h>
 
 @implementation ClassUtilities
+
++(void)ShowAlert:(NSString*)message
+{
+    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"WalGreens" message:message delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+    [alertView show];
+}
 
 @end
